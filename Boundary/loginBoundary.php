ba58e0820ed3267fcf72674c8cc68acb14731093
hwl96 <107758687+hwl96@users.noreply.github.com>
2022-11-04 19:24:56 +0800

    modified:   Boundary/loginBoundary.php

--- Boundary/loginBoundary.php
+++ Boundary/loginBoundary.php
@@ -1,4 +1,27 @@
 <?php
+    function displayLogin($arrayResult){
+        if($arrayResult){
+            session_start();
+            $_SESSION["username"] = $arrayResult['userName'];
+            $_SESSION["role"] = $arrayResult['role'];
+
+            switch ($_SESSION['role']){
+                case "Admin":
+                    header('Location: ../Html/AdminLogin.html');
+                    break;
+                case "Author":
+                    header('Location: ../Html/AuthorLogin.html');
+                    break;
+                case "Conference Chair":
+                    header('Location: ../Html/ConferenceLogin.html');
+                    break;
+                case "Reviewer":
+                    header('Location: ../Html/ReviewerLogin.html');
+                    break;
+            }
+        }
+    }
+    
     include'../Controller/doLoginController.php';
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -7,13 +30,5 @@
     $myArray = [$username, $password, $loginType];
     $loginControl = new loginController();
     $arrayResult = $loginControl -> passLoginPara($myArray);
-    
-    if($arrayResult){
-        session_start();
-        $_SESSION["username"] = $arrayResult['username'];
-        $_SESSION["role"] = $arrayResult['role'];
-    }
+    displayLogin($arrayResult);
 ?>
-<html>
-
-</html>
